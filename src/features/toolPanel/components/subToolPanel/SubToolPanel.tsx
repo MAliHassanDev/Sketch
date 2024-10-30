8d571f9b7fbcb27b657190edc60d9024cf31101f
Muhammad Ali Hassan <muhammadalihassanofficial@gmail.com>
2024-10-30 14:46:54 +0500

    feat: add sub-tool Panel for sub tools

---
+++ src/features/toolPanel/components/subToolPanel/SubToolPanel.tsx
@@ -0,0 +1,18 @@
+import ToolStack from "@/features/toolPanel/components/toolStack/ToolStack";
+import { ReactNode } from "react";
+import styles from "./SubToolPanel.module.css";
+
+type subToolPanelProps = {
+  children: ReactNode;
+};
+
+const SubToolPanel = ({ children }: subToolPanelProps) => {
+  return (
+    <div className={styles.subToolPanel}>
+      <ToolStack>{children}</ToolStack>
+    </div>
+  );
+};
+
+
+export default SubToolPanel;
