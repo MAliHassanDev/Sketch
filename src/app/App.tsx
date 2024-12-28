e11d99f64bc1e9e713411b862da6295b53942d23
ali <muhammadalihassanoffiial@gmail.com>
2024-12-28 10:30:15 +0500

    feat: add limit to undo and redo stack

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,47 +1,51 @@
 import styles from "./App.module.css";
 import { useContext, useState } from "react";
-import Canvas, {  Path } from "@/features/canvas/Canvas";
+import Canvas, { Path } from "@/features/canvas/Canvas";
 import ToolsProvider from "@/components/toolsProvider/ToolsProvider";
 import ThemeContext, { ThemeContextType } from "@/contexts/themeContext";
 import DrawToolPanel from "@/features/drawToolPanel/drawToolPanel";
 import UndoRedoPanel from "@/features/undoRedoPanel/UndoRedoPanel";
-import { removeLastArrayElement } from "@/utils/utils";
 import EraserCursor from "@/features/cursors/eraser/EraserCursor";
 import SwitchTheme from "@/features/switchTheme/switchTheme";
-import { getCanvasThemeColors, } from "@/utils/canvasToolUtils";
+import { getCanvasThemeColors } from "@/utils/canvasToolUtils";
 
 const App = () => {
-  const { theme,toggleTheme } = useContext(ThemeContext) as ThemeContextType;
-  const [undoStack, setUndoStack] = useState<Path[] | []>([]);
-  const [redoStack, setRedoStack] = useState<Path[] | []>([]);
+  const { theme, toggleTheme } = useContext(ThemeContext) as ThemeContextType;
+  const [undoStack, setUndoStack] = useState<Path[]>([]);
+  const [redoStack, setRedoStack] = useState<Path[]>([]);
   const [redrawCanvas, setRedrawCanvas] = useState<boolean>(false);
 
   function addNewPath(path: Path) {
-    setUndoStack([...undoStack, path]);
+    setUndoStack([...limitStack(undoStack), path]);
     setRedrawCanvas(false);
   }
 
+  function limitStack(stack: Path[]) {
+    const isStackOver = stack.length > 200;
+    return stack.filter((_, index) => {
+      return isStackOver ? index !== 0 : true;
+    });
+  }
   function handleUndo() {
     if (undoStack.length === 0) return;
-    const undoPath = undoStack[undoStack.length - 1];
-    setUndoStack(removeLastArrayElement(undoStack));
-    setRedoStack([...redoStack, undoPath]);
+    const undoPath = undoStack.pop() as Path;
+    setUndoStack([...undoStack]);
+    setRedoStack([...limitStack(redoStack), undoPath]);
     setRedrawCanvas(true);
   }
 
   function handleRedo() {
     if (redoStack.length === 0) return;
-    const redoPath = redoStack[redoStack.length - 1];
-    setRedoStack(removeLastArrayElement(redoStack));
-    if (!redoPath) return;
+    const redoPath = redoStack.pop() as Path;
+    setRedoStack([...redoStack]);
     setUndoStack([...undoStack, redoPath]);
     setRedrawCanvas(true);
   }
 
   function handleThemeSwitch() {
-    toggleTheme();  
-    const {dark,light } = getCanvasThemeColors();
-    const updatedUndoStack = undoStack.map(path => {
+    toggleTheme();
+    const { dark, light } = getCanvasThemeColors();
+    const updatedUndoStack = undoStack.map((path) => {
       if (path.strokeStyle == dark) {
         path.strokeStyle = light;
       } else if (path.strokeStyle === light) {
